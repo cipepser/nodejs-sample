416fe4c240a910392e815cf3cc6acbbd35512794
cipepser <respepic@gmail.com>
2017-07-26 22:22:05 +0900

    add switch statement

--- server.js
+++ server.js
@@ -1,9 +1,23 @@
 var http = require('http');
 var settings = require('./settings')
 var server = http.createServer();
+var msg;
+
 server.on('request', function(req, res) {
+  switch (req.url) {
+    case '/about':
+      msg = "about this page";
+      break;
+    case '/profile':
+      msg = "about me";
+      break;
+    default:
+      msg = "wrong page";
+      break;
+  }
+  
   res.writeHead(200, {'Content-Tyep': 'text/plain'});
-  res.write('hello world!!!');
+  res.write(msg);
   res.end();
 });
 
