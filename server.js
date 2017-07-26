3a2cd73285ff1b4dba8ed375b0f3d30f6d690b5f
cipepser <respepic@gmail.com>
2017-07-26 22:32:53 +0900

    read html

--- server.js
+++ server.js
@@ -1,24 +1,33 @@
-var http = require('http');
+var http = require('http'),
+    fs = require('fs');
 var settings = require('./settings')
 var server = http.createServer();
 var msg;
 
 server.on('request', function(req, res) {
-  switch (req.url) {
-    case '/about':
-      msg = "about this page";
-      break;
-    case '/profile':
-      msg = "about me";
-      break;
-    default:
-      msg = "wrong page";
-      break;
-  }
+  fs.readFile(__dirname + '/public_html/hello.html', 'utf-8', function(err, data) {
+    if (err) {
+      res.writeHead(404, {'Content-Tyep': 'text/html'});
+      res.write("not found!");
+      return res.end();
+    }
+    res.writeHead(200, {'Content-Tyep': 'text/html'});
+    res.write(data);
+    res.end();
+  });
   
-  res.writeHead(200, {'Content-Tyep': 'text/plain'});
-  res.write(msg);
-  res.end();
+  // switch (req.url) {
+  //   case '/about':
+  //     msg = "about this page";
+  //     break;
+  //   case '/profile':
+  //     msg = "about me";
+  //     break;
+  //   default:
+  //     msg = "wrong page";
+  //     break;
+  // }
+  
 });
 
 server.listen(settings.port, settings.host);
